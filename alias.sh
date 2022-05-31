9a7a1a5ea0df71269b98c8ac7519a9922a825ee3
Kevin Peña <kevin.pena.prog@gmail.com>
2022-05-30 21:35:36 -0500

    Cluster setup automatically

--- alias.sh
+++ alias.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+# This script requires
+# - terraform
+# - jq
+# - yq
+# - aws cli
+# - kubectl
+# - AWS credentials with a ton of permissions
+
 alias k='kubectl'
 alias tf='terraform -chdir=./tf'
 
@@ -16,4 +26,52 @@ logs() {
   local num="$1"
   shift
   aws ec2 get-console-output --instance-id "$(box_id "$num")" --output text "$@"
+}
+
+ssm-command() {
+  local id=$(box_id 1)
+  local res=$(aws ssm send-command \
+    --instance-ids "$id" \
+    --document-name "AWS-RunShellScript" \
+    --parameters "commands=$*")
+  local cmd_id=$(<<<"$res" jq '.Command.CommandId' -r)
+  while : ; do
+    local inv=$(aws ssm get-command-invocation --command-id "$cmd_id" --instance-id "$id")
+    [ "$(<<<"$inv" jq '.Status' -r)" == "InProgress" ] || break
+  done
+  printf 'Status: %s\n' "$(<<<"$inv" jq '.Status' -r)"
+  <<<"$inv" jq '.StandardErrorContent' -r
+  <<<"$inv" jq '.StandardOutputContent' -r
+  return $(<<<"$inv" jq '.ResponseCode' -r)
+}
+
+ssm-kubeconf() {
+  local ip=$(tf output -raw "kube1-ip")
+  local conf=$(ssm-command "kubectl config view --raw")
+  if [ -z "$conf" ]; then
+    >&2 echo "Empty conf $conf"
+    return
+  fi
+
+  local ca=$(<<<"$conf" yq '.clusters[0].cluster["certificate-authority-data"]')
+  local server="https://$ip:6443"
+  kubectl config set-cluster todo-kubernetes \
+    --server "$server" \
+    --embed-certs \
+    --certificate-authority <(base64 -d <<<"$ca")
+
+  local cert=$(<<<"$conf" yq '.users[0].user["client-certificate-data"]')
+  local key=$(<<<"$conf" yq '.users[0].user["client-key-data"]')
+  kubectl config set-credentials todo-kubernetes \
+    --embed-certs \
+    --client-certificate <(base64 -d <<<"$cert") \
+    --client-key <(base64 -d <<<"$key")
+
+  kubectl config set-context todo-kubernetes \
+    --cluster todo-kubernetes \
+    --user todo-kubernetes
+}
+
+kuse() {
+  kubectl config use todo-kubernetes
 }
--- tf/ec2.tf
+++ tf/ec2.tf
@@ -9,6 +9,45 @@ resource "aws_launch_template" "main" {
 
   network_interfaces {
     associate_public_ip_address = true
+    security_groups             = [aws_security_group.main.id]
+  }
+}
+
+resource "aws_security_group" "main" {
+  name        = "kube-cluster-node"
+  description = "Kube cluster node"
+  vpc_id      = aws_vpc.vpc.id
+
+  # Kube API
+  ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Http
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all communications between nodes
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    self      = true
+  }
+
+  # Allow outbound to internet
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
@@ -24,11 +63,17 @@ resource "aws_instance" "kube-1" {
 
   lifecycle {
     create_before_destroy = true
+    ignore_changes = [
+      iam_instance_profile,
+    ]
   }
 
   user_data = <<-EOT
   #!/bin/sh
   export K3S_TOKEN='${random_password.cluster_token.result}'
+  export AWS_TOKEN=$(curl -fsS "http://169.254.169.254/latest/api/token" -X PUT -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+  export PUBLIC_IP=$(curl -fsS "http://169.254.169.254/latest/meta-data/public-ipv4" -H "X-aws-ec2-metadata-token: $AWS_TOKEN")
+  export INSTALL_K3S_EXEC="--tls-san $PUBLIC_IP"
   curl -sfL https://get.k3s.io | sh -
   EOT
 }
@@ -41,6 +86,9 @@ resource "aws_instance" "kube-2" {
 
   lifecycle {
     create_before_destroy = true
+    ignore_changes = [
+      iam_instance_profile,
+    ]
   }
 
   user_data = <<-EOT
